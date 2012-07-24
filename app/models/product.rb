20be8b18ab2ee616f94b83014062600d39acd49a
Loveday <loveday.anyim@student.ncirl.ie>
2012-07-24 06:30:07 +0100

    Adding a Cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,4 +9,24 @@ class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
 
   validates :title, length: {minimum: 10}
+
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  #...
+
+
+  private
+
+  # ensure that there are no line items referencing this product
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'Line Items present')
+      return false
+    end
+  end
+
 end
